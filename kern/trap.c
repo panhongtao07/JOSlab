66ffe4c943477752ec253bf1fb6513e82188c3a6
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-11-15 00:02:26 +0800

    Lab3: ex6 断点异常分发
    - 断点异常分发

--- kern/trap.c
+++ kern/trap.c
@@ -174,6 +174,10 @@ trap_dispatch(struct Trapframe *tf)
 		page_fault_handler(tf);
 		return;
 	}
+	if (tf->tf_trapno == T_BRKPT) {
+		monitor(tf);
+		return;
+	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
 	print_trapframe(tf);
