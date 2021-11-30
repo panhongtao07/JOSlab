b380fe920ac22994b3c1efb556fcbf2981b12b28
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-12-01 00:15:54 +0800

    Lab4: ex8 pgfault upcall系统调用
    - 处理函数
    - 系统调用支持分配

--- kern/syscall.c
+++ kern/syscall.c
@@ -133,7 +133,11 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	struct Env *e; 
+	if (envid2env(envid, &e, 1) < 0)
+		return -E_BAD_ENV;
+	e->env_pgfault_upcall = func;
+	return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -343,6 +347,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
         break;
     case SYS_env_set_status:
         ret = sys_env_set_status(a1, a2);
+        break;
+    case SYS_env_set_pgfault_upcall:
+        ret = sys_env_set_pgfault_upcall(a1, (void *)a2);
         break;
     case SYS_page_alloc:
         ret = sys_page_alloc(a1, (void *)a2, (int)a3);
