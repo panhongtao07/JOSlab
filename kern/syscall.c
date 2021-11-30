b50257cbe4c8bb188f908894f14abcd64c3d09b7
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-11-30 21:17:30 +0800

    Lab4: ex6 yield
    - sched_yield
    - 系统调用
    - 用户

--- kern/syscall.c
+++ kern/syscall.c
@@ -285,6 +285,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
     case SYS_getenvid:
         ret = sys_getenvid();
         break;
+	case SYS_yield:
+		sys_yield();
+		break;
     default:
         ret = -E_INVAL;
     }
