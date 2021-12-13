f3aed608022f04ae2740163dce9e72bab68aeb53
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2021-12-13 22:59:52 +0800

    Lab5: ex4 文件块操作
    - file_block_walk获取文件某个块的实际块编号地址
    - file_get_block获取文件块的映射地址

--- fs/fs.c
+++ fs/fs.c
@@ -141,8 +141,26 @@ fs_init(void)
 static int
 file_block_walk(struct File *f, uint32_t filebno, uint32_t **ppdiskbno, bool alloc)
 {
-       // LAB 5: Your code here.
-       panic("file_block_walk not implemented");
+    // LAB 5: Your code here.
+    if (filebno >= NDIRECT + NINDIRECT)
+        return -E_INVAL;
+    
+    if (filebno < NDIRECT) {
+        *ppdiskbno = &(f->f_direct[filebno]);
+        return 0;
+    }
+    if (!f->f_indirect) {
+        if (alloc == false)
+            return -E_NOT_FOUND;
+        int r = alloc_block();
+        if (r < 0)
+            return -E_NO_DISK;
+        f->f_indirect = r;
+        memset(diskaddr(r), 0, BLKSIZE);
+        flush_block(diskaddr(r));
+    }
+    *ppdiskbno = (uint32_t *)diskaddr(f->f_indirect) + (filebno - NDIRECT);
+    return 0;
 }
 
 // Set *blk to the address in memory where the filebno'th
@@ -156,8 +174,20 @@ file_block_walk(struct File *f, uint32_t filebno, uint32_t **ppdiskbno, bool all
 int
 file_get_block(struct File *f, uint32_t filebno, char **blk)
 {
-       // LAB 5: Your code here.
-       panic("file_get_block not implemented");
+    // LAB 5: Your code here.
+    uint32_t *ppdiskbno;
+    int r = file_block_walk(f, filebno, &ppdiskbno, true);
+    if (r < 0)
+        return r;
+    if (*ppdiskbno == 0) {
+        if ((r = alloc_block()) < 0)
+            return -E_NO_DISK;
+        *ppdiskbno = r;
+        memset(diskaddr(r), 0, BLKSIZE);
+        flush_block(diskaddr(r));
+    }
+    *blk = diskaddr(*ppdiskbno);
+    return 0;
 }
 
 // Try to find a file named "name" in dir.  If so, set *file to it.
